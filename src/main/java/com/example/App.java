1ee0e9ec15597782cb9b93e17b88ad87f28a9afe
JBushek <61551672+JBushek@users.noreply.github.com>
2022-10-17 10:06:54 -0600

    Proper file read(Brkn)

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,7 +5,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Read.ReadJson();
+
     }
 
     public static void Some1(){
--- src/main/java/com/example/Read.java
+++ src/main/java/com/example/Read.java
@@ -1,10 +1,27 @@
 package com.example;
 
 import net.pwall.json.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Read
 {   
     public static void ReadJson(){
-
+        String jdata = "";
+        try {
+            File jsonF = new File("C:/Users/James Bushek/Documents/Class Work/OSPD/Project1/ssg_porject/resources");
+            Scanner fReader = new Scanner(jsonF);
+            while (fReader.hasNextLine()) {
+              jdata = fReader.nextLine();
+            }
+            fReader.close();
+          } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+          }
+        
+        Object jRaw = JSONSimple.parse(jdata);
+        System.out.println(jRaw);
     }
 }
