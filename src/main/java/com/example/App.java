0ae8bd7300a049f51bc5b783cb8fee167b104b86
JBushek <61551672+JBushek@users.noreply.github.com>
2022-10-18 10:21:31 -0600

    File Read Working

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,16 +1,24 @@
 package com.example;
-
+import org.json.simple.JSONArray;
 
 public class App 
 {
+    public String fullStatement;
+    public String fullHTML;
     public static void main( String[] args )
     {
-        Read.ReadJson();
-
+        int i = 0;
+        JSONArray jArray = Read.ReadJson();
+        //While(i<300){
+            Sort1(jArray, i);
+            i++;
+        //};
     }
 
-    public static void Some1(){
-        
+    public static void Sort1(JSONArray jArray, int loopNum){
+        String transfer = "[" + String.valueOf(jArray.get(loopNum)) + "]";
+        JSONArray indiArray = Read.ReadJsonPiece(transfer);
+        System.out.println(indiArray.get(0,0));
     }
 
     public static void Some2(){
--- src/main/java/com/example/Read.java
+++ src/main/java/com/example/Read.java
@@ -1,27 +1,47 @@
 package com.example;
 
-import net.pwall.json.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class Read
 {   
-    public static void ReadJson(){
+    public static JSONArray ReadJson(){
         String jdata = "";
         try {
-            File jsonF = new File("C:/Users/James Bushek/Documents/Class Work/OSPD/Project1/ssg_porject/resources");
+            File jsonF = new File("C:/Users/James Bushek/Documents/Class Work/OSPD/Project1/ssg_porject/resources/stock_transations-3.by.account.holder.json");
             Scanner fReader = new Scanner(jsonF);
             while (fReader.hasNextLine()) {
-              jdata = fReader.nextLine();
+              jdata = jdata + fReader.nextLine();
             }
             fReader.close();
           } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
           }
-        
-        Object jRaw = JSONSimple.parse(jdata);
-        System.out.println(jRaw);
+          JSONParser parser = new JSONParser();
+          JSONArray jArray;
+        try {
+            jArray = (JSONArray) parser.parse(jdata);
+            return jArray;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static JSONArray ReadJsonPiece(String jdata){
+        JSONParser parser = new JSONParser();
+        JSONArray jArray;
+        try {
+            jArray = (JSONArray) parser.parse(jdata);
+            return jArray;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
